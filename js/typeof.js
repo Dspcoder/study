fa216258d5396dc4da8931b847bc7651adaadb2d
Your Name <liuhong@qq.com>
2023-02-19 22:52:21 +0800

    typeof

---
+++ js/typeof.js
@@ -0,0 +1,27 @@
+/*
+typeof 只能模糊检测数据类型
+
+*/
+
+
+
+
+let a=[1,2,3]
+let b='123';
+let c={name:'wff'}
+let d=()=>{
+
+}
+let f =null;
+let e=undefined;
+let g=123
+let h=true
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(f));
+console.log(typeof(e));
+console.log(typeof(g));
+console.log(typeof(h));
+
